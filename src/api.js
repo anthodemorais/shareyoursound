d13ccca510d459f5365ab8cba5b4cdab3fbc16e8
Anthony <anthonyd-m@live.com>
2020-10-30 08:47:37 +0100

    :sparkles: created generic requests

--- src/api.js
+++ src/api.js
@@ -2,6 +2,7 @@ export default {
   baseUrl: 'https://api.shareyoursound.loscil.fr/api',
   headers: { 'Content-Type': 'application/json' },
   headersLogged: this.headers + { Authorization: `Bearer ${localStorage.getItem('token')}` },
+
   login(email, password, completion) {
     const body = { username: email, password };
     fetch(`${this.baseUrl}/login_check`, {
@@ -12,7 +13,7 @@ export default {
         completion();
       });
     }).catch((error) => {
-      console.log(error);
+      console.log(error.message);
     });
   },
 
@@ -20,7 +21,6 @@ export default {
     const body = {
       email, password, firstname, lastname,
     };
-
     fetch(`${this.baseUrl}/register`, {
       method: 'POST', headers: this.headers, body,
     }).then(() => {
@@ -28,7 +28,42 @@ export default {
         completion();
       });
     }).catch((error) => {
-      console.log(error);
+      console.log(error.message);
+    });
+  },
+
+  getRequest(url, completion) {
+    fetch(`${this.baseUrl}${url}`, {
+      headers: this.headersLogged,
+    }).then((res) => {
+      res.json().then((data) => {
+        completion(data);
+      });
+    }).catch((error) => {
+      console.log(error.message);
+      completion('error');
+    });
+  },
+
+  postOrPutRequest(method, url, body, completion) {
+    fetch(`${this.baseUrl}${url}`, {
+      method, headers: this.headersLogged, body,
+    }).then(() => {
+      completion();
+    }).catch((error) => {
+      console.log(error.message);
+      completion('error');
+    });
+  },
+
+  deleteRequest(url, completion) {
+    fetch(`${this.baseUrl}${url}`, {
+      method: 'DELETE', headers: this.headersLogged,
+    }).then(() => {
+      completion();
+    }).catch((error) => {
+      console.log(error.message);
+      completion('error');
     });
   },
 
