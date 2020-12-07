cf11c03fcdcbc3bb82a2ef4ce63497f289105bd4
Spyynox <maxime.mbiya-kiaku@supinternet.fr>
2020-12-07 08:38:07 +0100

    🐛 fix bug API call

--- src/views/MediaID.vue
+++ src/views/MediaID.vue
@@ -1,30 +1,28 @@
 <template>
-    <div v-if="medium.length !== 0">
-        <h1>{{ medium.name }}</h1>
-        <audio controls="controls">
-            <source v-bind:src="medium.file" type="audio/mp3" />
-        </audio>
-        <div>Crée par :</div>
+    <h1>{{ medium.name }}</h1>
+    <audio controls="controls">
+        <source v-bind:src="medium.file" type="audio/mp3" />
+    </audio>
+    <div>Crée par :</div>
 
-        <img v-bind:src="medium.author.picture" />
-        <div>
-            <a v-bind:href="url + medium.author.id">
-                {{ medium.author.firstname }} {{ medium.author.lastname }}
+    <img v-bind:src="picture" />
+    <div>
+        <a v-bind:href="url + id">
+            {{ firstname }} {{ lastname }}
+        </a>
+    </div>
+    <h2>Son/Ses genres : </h2>
+    <div v-for="type in types" :key="type.id" >
+        <h3>{{type.name}}</h3>
+    </div>
+    <AddTypeToMedia />
+    <h2>Ceux qui aiment cette musique: </h2>
+    <div v-for="liker in likers" :key="liker.id" >
+        <span>
+            <a v-bind:href="url + liker.id">
+                {{ liker.firstname }} {{ liker.lastname }}
             </a>
-        </div>
-        <h2>Son/Ses genres : </h2>
-        <div v-for="type in types" :key="type.id" >
-            <h3>{{type.name}}</h3>
-        </div>
-        <AddTypeToMedia v-bind:media="id" />
-        <h2>Ceux qui aiment cette musique: </h2>
-        <div v-for="liker in likers" :key="liker.id" >
-            <span>
-                <a v-bind:href="url + liker.id">
-                    {{ liker.firstname }} {{ liker.lastname }}
-                </a>
-            </span>,
-        </div>
+        </span>
     </div>
 </template>
 
@@ -34,14 +32,26 @@ import { useRouter } from 'vue-router';
 import AddTypeToMedia from '@/components/AddTypeToMedia.vue';
 import api from '../api';
 
-function useMediaID(id) {
+function useMediaID(userId) {
   const url = '/profile/';
   const medium = ref([]);
   const likers = ref([]);
   const types = ref([]);
+  const id = ref('');
+  const name = ref('');
+  const file = ref('');
+  const picture = ref('');
+  const firstname = ref('');
+  const lastname = ref('');
 
-  api.getRequest(`/media/${id}`, (data) => {
+  api.getRequest(`/media/${userId}`, (data) => {
     if (data !== 'error') {
+      name.value = data.medium.name;
+      file.value = data.medium.file;
+      id.value = data.medium.author.id;
+      picture.value = data.medium.author.picture;
+      firstname.value = data.medium.author.firstname;
+      lastname.value = data.medium.author.lastname;
       medium.value = data.medium;
       likers.value = data.medium.likers;
       types.value = data.medium.types;
@@ -49,7 +59,7 @@ function useMediaID(id) {
   });
 
   return {
-    url, medium, likers, types, id,
+    name, file, picture, firstname, lastname, url, medium, likers, types, id,
   };
 }
 export default defineComponent({
